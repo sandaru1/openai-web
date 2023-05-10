f51b8b7f303ed157276f06bbcde245f3fdd8557e
Sandaruwan Gunathilake <sandaruwan@gunathilake.com>
2023-05-10 18:59:53 +0530

    Added a module output for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,53 @@
 const path = require('path');
 
-module.exports = {
-    entry: './index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
+module.exports = [
+    {
+        entry: './index.ts',
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: 'openai.min.js',
+            path: path.resolve(__dirname, 'dist'),
+            globalObject: 'this',
+            library: {
+                name: 'OpenAI',
+                type: 'umd',
+            }
+        }
+    },
+    {
+        entry: './index.ts',
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: 'openai.module.min.js',
+            path: path.resolve(__dirname, 'dist'),
+            library: {
+                type: 'module',
             },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'openai.min.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'OpenAI'
-    }
-};
+        },
+        experiments: {
+            outputModule: true
+        }
+    }    
+];
